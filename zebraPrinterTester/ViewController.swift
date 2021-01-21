ec81b20ba141452a234949075db72055bf6e878c
Fernando <hcm415@ha.org.hk>
2021-01-21 12:16:40 +0800

    Initial Commit

---
+++ zebraPrinterTester/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zebraPrinterTester
+//
+//  Created by Fernando on 21/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
